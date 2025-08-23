f4f0a3dad4322a95238e942f940b712feae6f958
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2025-08-23 19:03:12 +0200

    Remove unnecessary comments from recipe card click event handler

--- recipes/all recipes/app.js
+++ recipes/all recipes/app.js
@@ -1,9 +1,7 @@
-// Selektuj sve kartice
 const cards = document.querySelectorAll(".card");
 
 cards.forEach((card, index) => {
   card.addEventListener("click", () => {
-    // Dodeljujemo ID recepta (isti redosled kao u JSON-u)
     const recipeIds = [
       "oreo_cupcakes",
       "blueberry_peach_pie",
@@ -16,7 +14,6 @@ cards.forEach((card, index) => {
 
     const recipeId = recipeIds[index];
 
-    // Prebacujemo ID recepta u URL
     window.location.href = `recipe.html?id=${recipeId}`;
   });
 });

2d8efe1d9fb5e56fdb908e9bf9dca6560573f3f7
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2025-09-06 15:42:23 +0200

    Implement modal functionality for adding recipes with open, close, and save actions

--- recipes/all recipes/app.js
+++ recipes/all recipes/app.js
@@ -17,3 +17,28 @@ cards.forEach((card, index) => {
     window.location.href = `recipe.html?id=${recipeId}`;
   });
 });
+
+const openBtn = document.getElementById("addRecipeBtn");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".modal-content .close");
+const saveRecipe = document.getElementById("saveRecipe");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.classList.remove("active");
+  }
+});
+
+saveRecipe.addEventListener("click", () => {
+  const div = document.createElement("div");
+  const title = document.createElement("div");
+  const text = document.createElement("div");
+});
